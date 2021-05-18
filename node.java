090001123d414acea963008accd3ca6d9bc7f6a1
Garima Agarwal <69511820+GarimaAgarwal515@users.noreply.github.com>
2021-05-18 15:14:32 +0530

    Add files via upload

---
+++ node.java
@@ -0,0 +1,12 @@
+package BinarySearchTree;
+
+public class node {
+    int data;
+    node right;
+    node left;
+    public node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+    }
+
